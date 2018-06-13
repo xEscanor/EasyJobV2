5480127e2097ba799e2ee32be35885ec70bb35b8
Clément <clement.dugord@gmail.com>
2018-06-13 20:15:31 +0200

    Ajout du swipe jobSeeker/JobOffer Modification de la webUI en bootstrap

---
+++ EasyJob/Scripts/custom.js
@@ -0,0 +1,46 @@
+﻿$(document).ready(function () {
+    console.log('execute JQuery');
+    $(".buddys").on("swiperight", function () {
+        swipeRightAction($(this));
+    });
+
+    $(".buddys").on("swipeleft", function () {
+        swipeLeftAction($(this));
+    });
+    $(".likeBtn").click(function () {
+        console.log($(this).attr('id') );
+        swipeRightAction($("#buddy" + $(this).attr('id')));
+    });
+    $(".dissBtn").click(function () {
+        console.log($(this).attr('id') );
+        swipeLeftAction($("#buddy" + $(this).attr('id')));
+    });
+
+
+});
+function swipeRightAction(buddy) {
+    console.log('moveRight');
+    $(buddy).addClass('rotate-left').delay(700).fadeOut(1);
+    $('.buddys').find('.status').remove();
+
+    $(buddy).append('<div class="status like">Like!</div>');
+    if ($(buddy).is(':last-child')) {
+        $('.buddys:nth-child(1)').removeClass('rotate-left rotate-right').fadeIn(300);
+    } else {
+        $(buddy).next().removeClass('rotate-left rotate-right').fadeIn(400);
+    }
+}
+
+function swipeLeftAction(buddy) {
+    console.log('moveLeft');
+    $(buddy).addClass('rotate-right').delay(700).fadeOut(1);
+    $('.buddys').find('.status').remove();
+    $(buddy).append('<div class="status dislike">Dislike!</div>');
+
+    if ($(buddy).is(':last-child')) {
+        $('.buddys:nth-child(1)').removeClass('rotate-left rotate-right').fadeIn(300);
+        alert('OUPS');
+    } else {
+        $(buddy).next().removeClass('rotate-left rotate-right').fadeIn(400);
+    }
+}
