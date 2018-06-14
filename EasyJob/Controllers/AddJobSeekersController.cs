1422c9fc920ac995b2449b28ff3e313d36aaf18a
de oliveira cyril <cyril-91270@hotmail.fr>
2018-06-14 09:18:22 +0200

    oui

--- EasyJob/Controllers/AddJobSeekersController.cs
+++ EasyJob/Controllers/AddJobSeekersController.cs
@@ -16,8 +16,8 @@ namespace EasyJob.Controllers
 
          public ActionResult Index()
         {
-            var session = Convert.ToInt32(Session["userID"]);
-            return View(db.JobSeekers.Where(x => x.Id == session).ToList());
+            return View(db.JobSeekers.ToList());
+        }
 
             //var result = db.JobOffers.Where(x => x.Company.UserId == Session["userID"]).ToList();
           //  if (result == null)
@@ -33,7 +33,7 @@ namespace EasyJob.Controllers
         }
 
         [HttpPost]
-
+        [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "Id,Email,Username,LastName,FirstName,BirthDay,Age,Diploma,Experience,Gender")] JobSeeker jobSeeker)
         {
             JobSeeker jobseeker = (from c in db.JobSeekers
@@ -48,6 +48,69 @@ namespace EasyJob.Controllers
             // }
             Console.WriteLine("oui");
             return View(jobSeeker);
+        }
+
+ 
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            JobSeeker jobSeeker = db.JobSeekers.Find(id);
+            if (jobSeeker == null)
+            {
+                return HttpNotFound();
+            }
+            return View(jobSeeker);
+        }
+
+    
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Email,Username,LastName,FirstName,BirthDay,Age,Diploma,Experience,Gender")] JobSeeker jobSeeker)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(jobSeeker).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(jobSeeker);
+        }
+        
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            JobSeeker jobSeeker = db.JobSeekers.Find(id);
+            if (jobSeeker == null)
+            {
+                return HttpNotFound();
+            }
+            return View(jobSeeker);
+        }
+
+        
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            JobSeeker jobSeeker = db.JobSeekers.Find(id);
+            db.JobSeekers.Remove(jobSeeker);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
